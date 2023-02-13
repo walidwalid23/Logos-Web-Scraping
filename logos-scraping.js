b1cb72c9b85019155f6c789b54570c225b78d544
Walid <walid.tawfik200@gmail.com>
2023-02-14 00:47:18 +0200

    keep alive2

--- logos-scraping.js
+++ logos-scraping.js
@@ -18,7 +18,8 @@ app.get('/WalidLogosApi', async function (req, res) {
     let isLastPage = false;
     console.log("received request");
     // set transfer encoding header as chunked since its a stream
-    res.setHeader('Transfer-Encoding', 'chunked');
+    // res.setHeader('Transfer-Encoding', 'chunked');
+    res.set('Connection', 'keep-alive');
 
     while (!isLastPage) {
         // const readable = new Stream.Readable({ objectMode: true })
