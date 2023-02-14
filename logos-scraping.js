65532e619c7340991e4e86a1ce9fbfba3b059867
Walid <walid.tawfik200@gmail.com>
2023-02-14 15:54:53 +0200

    text/html content type

--- logos-scraping.js
+++ logos-scraping.js
@@ -18,9 +18,7 @@ app.get('/WalidLogosApi', async function (req, res) {
     let isLastPage = false;
     console.log("received request");
     // set transfer encoding header as chunked since its a stream
-    // res.setHeader('Transfer-Encoding', 'chunked');
-    res.set('Connection', 'keep-alive');
-    res.setHeader('Connection', 'keep-alive');
+    res.writeHead(200, { "Content-Type": "text/html" });
 
     while (!isLastPage) {
         // const readable = new Stream.Readable({ objectMode: true })
